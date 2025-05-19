b79b4b9ee7d5ba2777638afad155e7b6fc74ca79
deepfutur <mayatchidraw@gmail.com>
2025-05-19 17:59:26 +0200

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { FC } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster as AppToaster } from "@/components/ui/toaster";
+import { Toaster as SonnerToaster } from "@/components/ui/sonner";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
+const App: FC = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
+      <AppToaster />
+      <SonnerToaster />
       <BrowserRouter>
         <div className="min-h-screen bg-pos-background">
           <Routes>
-            <Route path="/*" element={<Index />} />
+            <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
